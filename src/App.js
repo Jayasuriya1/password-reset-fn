ddb2af146b1a7008f2b7bde51ec33aac076b6e8a
jayasuriya1 <jaya1999suriya1@gmail.com>
2023-06-06 01:43:24 +0530

    default page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Login from './component/login';
-import { Routes, Route,Redirect} from "react-router-dom";
+import { Routes, Route,Navigate} from "react-router-dom";
 import Register from './component/register';
 import DashBoard from './component/dashboard';
 import ForgetPassword from './component/forgetPassword';
@@ -9,17 +9,13 @@ import ResetPassword from './component/resetPassword';
 function App() {
   return (
     <Routes>
-      <Route  path='/login' element={<Login/>}/>
+      <Route path='/login' element={<Login/>}/>
       <Route path='/register' element={<Register/>}/>
       <Route path='/dashboard' element={<DashBoard/>}/>
       <Route path='/forgetPassword' element={<ForgetPassword/>}/>
       <Route path='/resetPassword/:id/:token' element={<ResetPassword/>}/>
-      <Route exact path="/">
-          <Redirect to="/login" />
-      </Route>
-      <Route path="*">
-          <Redirect to="/login" />
-      </Route>
+      <Route path='/' element={<Navigate to="/login"/>}/>
+      <Route path='*' element={<Navigate to="/login"/>}/>
     </Routes>
   );
 }
